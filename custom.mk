f6d89fba2a04e83f2517fa39eb66890a31af2c61
esix <esix@live.ru>
2026-06-19 17:02:33 +0300

    build: relax clang flags for modern toolchains

--- custom.mk
+++ custom.mk
@@ -1,1 +1,3 @@
-
+# Local build settings for lcc-wasm.
+# Modern clang treats K&R implicit declarations / int<->ptr as hard errors; relax them.
+CFLAGS=-g -O0 -Wno-implicit-function-declaration -Wno-int-conversion -Wno-implicit-int -Wno-error -Wno-parentheses -Wno-return-type
